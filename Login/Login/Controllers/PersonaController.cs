8e6758f825aebb15341b299da25f884b43e0e4cd
William Diaz <williamefrain1994@gmail.com>
2021-08-28 13:53:52 -0600

    GO

---
+++ Login/Login/Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult procesar(String usuario, String DUI) {
+
+            if (usuario.Equals("William")&& DUI.Equals("12345678")) {
+                return Redirect("/Persona/segundavista");
+            }
+            return Redirect("/Persona/Index");
+        }
+
+        public ActionResult segundavista() {
+
+            return View();
+        }
+
+       
+        public ActionResult MostrarDatosPrecesados(String nombre, String hrstrab, String antiguedad, String valorH) {
+
+
+            return View();
+        }
+    }
+}
